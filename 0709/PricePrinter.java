696eafa77ddd43f3189d50b8c3dcb17b665dd90e
garagara4145 <155130874+garagara4145@users.noreply.github.com>
2026-07-09 15:49:16 +0800

    Add 0709 method practice

---
+++ 0709/PricePrinter.java
@@ -0,0 +1,12 @@
+public class PricePrinter {
+
+    public static void main(String[] args ){
+         printItem("A", 100);
+        printItem("B", 250);
+    }
+   public static void printItem(String itemName, int price){
+        System.err.println("String itemName"+price);
+
+    }
+    
+}
