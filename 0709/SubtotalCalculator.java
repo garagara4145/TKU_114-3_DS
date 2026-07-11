e3737fb03da3f4d6e13b7a790de8940811e2d94e
garagara4145 <155130874+garagara4145@users.noreply.github.com>
2026-07-11 23:35:11 +0800

    Add 0709 ATM menu homework

--- 0709/SubtotalCalculator.java
+++ 0709/SubtotalCalculator.java
@@ -1,3 +1,12 @@
 public class SubtotalCalculator {
-    
+    public static void main(String[] args) {
+        int subtotal = calculateSubtotal(120, 3);
+
+     
+        System.out.println("商品小計：" + subtotal);
+    }
+
+    public static int calculateSubtotal(int price, int quantity) {
+        return price * quantity;
+    }
 }
